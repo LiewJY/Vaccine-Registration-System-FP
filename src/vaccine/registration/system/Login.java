07def2943d9284897f3adce59a7c53410b46a47f
boeyjunhui <boeyjunhui@localhost>
2021-10-27 18:10:13 +0800

    Designed Login Form & People Register Account Form. Added 2 logos.

--- src/vaccine/registration/system/Login.java
+++ src/vaccine/registration/system/Login.java
@@ -26,20 +26,107 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pnl_login = new javax.swing.JPanel();
+        lbl_logo = new javax.swing.JLabel();
+        lbl_ic_passport_number = new javax.swing.JLabel();
+        txt_ic_passport_number = new javax.swing.JTextField();
+        lbl_password = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        cbo_user_role = new javax.swing.JComboBox<>();
+        btn_login = new javax.swing.JButton();
+        btn_register = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        pnl_login.setBackground(new java.awt.Color(255, 255, 255));
+        pnl_login.setPreferredSize(new java.awt.Dimension(1024, 576));
+
+        lbl_logo.setBackground(new java.awt.Color(255, 255, 255));
+        lbl_logo.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        lbl_logo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lbl_logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vaccine/registration/system/images/MyVaccine Logo - Blue.png"))); // NOI18N
+
+        lbl_ic_passport_number.setBackground(new java.awt.Color(255, 255, 255));
+        lbl_ic_passport_number.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        lbl_ic_passport_number.setText("IC / Passport Number");
+
+        txt_ic_passport_number.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+
+        lbl_password.setBackground(new java.awt.Color(255, 255, 255));
+        lbl_password.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        lbl_password.setText("Password");
+
+        jPasswordField1.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+
+        cbo_user_role.setBackground(new java.awt.Color(255, 255, 255));
+        cbo_user_role.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        cbo_user_role.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select User Role", "Personnel", "People (Citizen / Non-Citizen)" }));
+
+        btn_login.setBackground(new java.awt.Color(73, 161, 236));
+        btn_login.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        btn_login.setForeground(new java.awt.Color(255, 255, 255));
+        btn_login.setText("Log In");
+        btn_login.setBorder(null);
+
+        btn_register.setBackground(new java.awt.Color(255, 255, 255));
+        btn_register.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        btn_register.setText("Register");
+
+        javax.swing.GroupLayout pnl_loginLayout = new javax.swing.GroupLayout(pnl_login);
+        pnl_login.setLayout(pnl_loginLayout);
+        pnl_loginLayout.setHorizontalGroup(
+            pnl_loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnl_loginLayout.createSequentialGroup()
+                .addGap(312, 312, 312)
+                .addGroup(pnl_loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(pnl_loginLayout.createSequentialGroup()
+                        .addComponent(btn_login, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                        .addComponent(btn_register, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cbo_user_role, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lbl_password)
+                    .addComponent(lbl_ic_passport_number)
+                    .addComponent(txt_ic_passport_number)
+                    .addComponent(lbl_logo, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jPasswordField1))
+                .addContainerGap(312, Short.MAX_VALUE))
+        );
+        pnl_loginLayout.setVerticalGroup(
+            pnl_loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnl_loginLayout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(lbl_logo, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(60, 60, 60)
+                .addComponent(lbl_ic_passport_number)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txt_ic_passport_number, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(lbl_password)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(cbo_user_role, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(80, 80, 80)
+                .addGroup(pnl_loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_login, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn_register, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(79, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(pnl_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(pnl_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -78,5 +165,14 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_login;
+    private javax.swing.JButton btn_register;
+    private javax.swing.JComboBox<String> cbo_user_role;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JLabel lbl_ic_passport_number;
+    private javax.swing.JLabel lbl_logo;
+    private javax.swing.JLabel lbl_password;
+    private javax.swing.JPanel pnl_login;
+    private javax.swing.JTextField txt_ic_passport_number;
     // End of variables declaration//GEN-END:variables
 }
