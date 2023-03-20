3d1cf8ad90f8550bcab88901a4b68410e38d8a51
liewjy <liewjy@localhost>
2023-03-20 21:41:57 +0800

    temp

---
+++ src/vaccine/registration/system/CenterRecord.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vaccine.registration.system;
+
+/**
+ *
+ * @author liewjy
+ */
+record CenterRecord(int Center_ID, String Center_Name, String Center_Address, String Center_Contact_Number, String Vaccine_Type) {
+
+}
