fd5e53e7a807ce143a70adde3afbb78e9ebf078f
LiewJY <liewjunyoung@gmail.com>
2023-03-24 10:12:14 +0800

    add functional interface

--- src/vaccine/registration/system/PeopleController.java
+++ src/vaccine/registration/system/PeopleController.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,15 +22,18 @@ import java.util.stream.Stream;
  */
 public class PeopleController {
 
+    Function<Integer, Integer> idIncrement = x -> x + 1;
+
     public int calculatePeople_ID() throws FileNotFoundException, IOException {
         BufferedReader reader = new BufferedReader(new FileReader("People.txt"));
         int id
-        = reader.lines().map(line -> line.split("//"))
-                .map(data -> new PeopleRecord(Integer.parseInt(data[0]), data[1], data[2], data[3], data[4], data[5], data[6]))
-                .reduce((first, second) -> second)
-                .get()
-                .People_ID();
-        return id + 1;
+                = reader.lines().map(line -> line.split("//"))
+                        .map(data -> new PeopleRecord(Integer.parseInt(data[0]), data[1], data[2], data[3], data[4], data[5], data[6]))
+                        .reduce((first, second) -> second)
+                        .get()
+                        .People_ID();
+
+        return idIncrement.apply(id);
     }
 
 }
